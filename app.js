27c6930c146325a58bc9e0f37123f47e2c55dba7
SNBT5T <SNBT5T@neptun.gde.hu>
2025-12-06 18:29:20 +0100

    WeatherPing initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("WeatherPing API is running – Sunny 25°C\n");
+});
+
+server.listen(PORT, () => {
+  console.log(`WeatherPing listening on http://localhost:${PORT}`);
+});
